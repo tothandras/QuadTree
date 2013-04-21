079d137ab6af4fbca2b24a61da625f5218ecd02f
Tóth András <bfme2hun@gmail.com>
2013-04-21 22:01:29 +0200

    Kommentelve

--- QuadTree.hpp
+++ QuadTree.hpp
@@ -7,35 +7,52 @@
 #define __NHF___QuadTree__QuadTree__
 
 #include <iostream>
+
 template <typename T>
 class QuadTree;
 
 template <typename T>
+class QuadTreeNode;
+
+template <typename T>
+std::ostream& operator<<(std::ostream &,const QuadTree<T> &);
+
+template <typename T>
+std::ostream& operator<<(std::ostream &,const QuadTreeNode<T> &);
+
+template <typename T>
 class QuadTreeNode{
-    // szülő
+    /// Szülő.
     QuadTreeNode *parent;
-    // 4 gyerek
+    /// Négy gyerek.
     QuadTreeNode *children[4];
-    // generikus adat
+    /// Adat.
     T data;
-    // van-e adat? adattal lett létrehozva?
+    /// Van-e adattagja?
     bool hasdata;
-    // szint
+    /// Fában lévő szintje
     unsigned int level;
-    // nincs alapértelmezett konstruktora
+    /// Nincs paraméter nélküli konstruktora.
     QuadTreeNode();
-    // és csak a QuadTree tud létrehozni újabb csomópontot
-    QuadTreeNode(unsigned int level, QuadTreeNode *parent=NULL): parent(parent), children{NULL, NULL, NULL, NULL}, hasdata(false), level(level){}
-    QuadTreeNode(T Data, unsigned int level, QuadTreeNode *parent=NULL): parent(parent), children{NULL, NULL, NULL, NULL}, data(Data), hasdata(true),level(level){}
-    // ha delete NULL hívódna meg, az nem gond, ezért nem vizsgálja
+    /// QuadTree hozhat létre új csomópontot. (Adat nélkül)
+    QuadTreeNode(unsigned int level=0, QuadTreeNode *parent=NULL): parent(parent), children{NULL, NULL, NULL, NULL}, hasdata(false), level(level){}
+    /// QuadTree hozhat létre új csomópontot. (Adattal)
+    QuadTreeNode(T Data, unsigned int level=0, QuadTreeNode *parent=NULL): parent(parent), children{NULL, NULL, NULL, NULL}, data(Data), hasdata(true),level(level){}
+    /// Destruktor. (delete NULL; esetén nincs probléma, nem kell vizsgálni)
     ~QuadTreeNode(){
         for (size_t i=0; i<4; ++i)
             delete children[i];
     }
 public:
+    /// @return - A csomópont rendelkezik-e adattal?
     bool hasData(){return hasdata;}
-    T& getData(){return data;}
+    /// @return - Adat
+    T& getData(){
+        if (hasData())
+            return data;}
+    /// @return - Melyik szinten van?
     unsigned int getLevel(){return level;}
+    /// Gyerekek törlése úgy, hogy a gyerekek gyerekei is törlődnek.
     void deleteChildren(){
         if (*this==NULL)
             return;
@@ -46,14 +63,19 @@ public:
             }
         delete data;
     }
+    /// Adat beszúrása.
+    /// @param - Adat
     void insert(T data){
         size_t i=0;
         while(children[i]!=NULL && i<4)
             ++i;
         children[i]=new QuadTreeNode(data, level+1, this);
     }
+    /// Csopontról eldönti hogy levél-e.
+    /// @return - Levél?
     bool isLeaf() const{return (children[0]==NULL && children[1]==NULL && children[2]==NULL && children[3]==NULL);}
-    // rekurzív megoldás
+    /// Fa/részfa mélységének visszaadása rekurzív függvényhívással.
+    /// @return - Fa/részfa mélysége
     unsigned int depth() const{
         unsigned int depth[4], max=0;
         if(this==NULL) return 0;
@@ -64,29 +86,42 @@ public:
                 max=depth[i];
         return max+1;
     }
-    // reszfaban levo elemek megszamolasa
+    /// Fában/részfában lévő elemek megszámolása rekurzív függvényhívással.
+    /// @return - Fa/részfa elemszáma
     unsigned int countElements() const{
         if (this==NULL) return 0;
         return children[0]->countElements()+children[1]->countElements()+children[2]->countElements()+children[3]->countElements()+ 1;
     }
+    /// Fában/részfában lévő levelek megszámolása rekurzív függvényhívással.
+    /// @return - Fa/részfa levélszáma
     unsigned int countLeafs() const{
         if (this==NULL) return 0;
         if (isLeaf()) return 1;
         return children[0]->countLeafs()+children[1]->countLeafs()+children[2]->countLeafs()+children[3]->countLeafs();
     }
+    
     friend class QuadTree<T>;
-    template <typename t>
-    friend std::ostream& operator<<(std::ostream & os, const QuadTreeNode<t> & Node);
+    friend std::ostream& operator<< <T>(std::ostream &, const QuadTreeNode<T> &);
 };
 
 template <typename T>
 class QuadTree{
+    /// Fa gyökerére mutató pointer.
     QuadTreeNode<T> *root;
 public:
+    /// Konstruktor. (Adat nélkül)
+    QuadTree(){
+        root = new QuadTreeNode<T>();
+    }
+    /// Konstruktor. (Adattal)
     QuadTree(T Data){
         root = new QuadTreeNode<T>(Data, 0);
     }
+    /// Destruktor.
     ~QuadTree(){delete root;}
+    /// @return - Gyökérre mutató pointer.
+    QuadTreeNode<T>* getRootNode(){return root;}
+    /// Új elem beszúrása
     void insert(T data){
         QuadTreeNode<T>* temp=root;
         unsigned int elements[4], min;
@@ -112,16 +147,20 @@ public:
             }
         }
     }
+    /// Fa mélységének visszaadása rekurzív függvényhívással.
+    /// @return - Fa mélysége
     unsigned int depth(){return root->depth();}
+    
+    /// Fa iterátor.
     class iterator;
     iterator begin(){}
     iterator end(){}
-    QuadTreeNode<T>* getRootNode(){return root;}
+    
     friend class QuadTreeNode<T>;
-    template <typename t>
-    friend std::ostream& operator<<(std::ostream &,const QuadTree<t> &);
+    friend std::ostream& operator<< <T>(std::ostream &,const QuadTree<T> &);
 };
 
+/* Munka alatt */
 template <class T>
 class QuadTree<T>::iterator{
 protected:
@@ -141,6 +180,7 @@ public:
     }
 };
 
+/// Fa kiírása rekurzívan.
 template <typename T>
 std::ostream& operator<<(std::ostream & os, const QuadTree<T> & QuadTree){
     if (QuadTree.root==NULL)
@@ -148,7 +188,8 @@ std::ostream& operator<<(std::ostream & os, const QuadTree<T> & QuadTree){
     os << *QuadTree.root;
     return os;
 }
-    
+
+/// Részfa kiírása rekurzívan
 template <typename T>
 std::ostream& operator<<(std::ostream & os, const QuadTreeNode<T> & Node){
     if (&Node==NULL)
